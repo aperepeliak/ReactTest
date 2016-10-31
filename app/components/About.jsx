cd3893ac3b0b7c24905a0f7116cace88ce6a5890
aperepeliak <aperepelyak@gmail.com>
2016-10-31 12:07:02 +0200

    change about comp

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,7 +12,10 @@ let React = require('react');
 //Stateless functional component - such components do not maintain state and have only render function
 let About = (props) => {
     return (
-        <h3>About component</h3>
+        <div>
+            <h3>About component</h3>
+            <p>Welcome to about page</p>
+        </div>
     );
 };
 
