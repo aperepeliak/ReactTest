25f4a429c59da5d6587764b99dc270dc65b82b5e
aperepeliak <aperepelyak@gmail.com>
2016-11-01 21:17:40 +0200

    fix modal error window

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -21,7 +21,7 @@ let Nav = React.createClass({
             <div className="top-bar">
                 <div className="top-bar-left">
                     <ul className="menu">
-                        <li className="menu-text">Weather App</li>
+                        <li className="menu-text logo">Weather App</li>
                         <li>
                             <IndexLink to="/" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Get Weather</IndexLink>
                         </li>
